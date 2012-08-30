06cfbb75ca4f74c6ad1bc9c26d3e45e46af2aea0
Eric Davis <ed@npri.org>
2012-08-30 09:19:04 -0700

    Include keepalives

--- tweetnevada_updates.py
+++ tweetnevada_updates.py
@@ -1,3 +1,4 @@
+import subprocess
 from munin.mongodb import MuninMongoDBPlugin
 
 class TweetNevadaUpdates(MuninMongoDBPlugin):
@@ -16,10 +17,20 @@ class TweetNevadaUpdates(MuninMongoDBPlugin):
             ("trusted",
              dict(label="trusted updates", info="number of trusted updates", type="COUNTER")),
 
+            ("keepalive",
+             dict(label="keepalives", info="number of keepalive notices", type="COUNTER"))]
+
     def execute(self):
         db = self.connection['tweetnv']
         print "trusted.value %d" % db.trusted_updates.find().count()
         print "untrusted.value %d" % db.untrusted_updates.find().count()
+
+        log = '/var/log/supervisor/tweetnevada.log'
+        cmd = ["grep", "-c", "keepalive", log]
+        status = subprocess.Popen(cmd, stdout=subprocess.PIPE)
+        (stdout, stderr) = status.communicate()
+        keepalive_count = int(stdout.strip())
+        print "keepalive.value %d" % keepalive_count
 
 def main():
     TweetNevadaUpdates().run()
